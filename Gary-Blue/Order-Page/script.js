8795854752d93ba698ca084f9cd218d985112158
Tarsolan <alex.ridgeley@keyin.com>
2022-03-07 15:16:53 -0330

    Updated Order Page with "Add Item"

--- Gary-Blue/Order-Page/script.js
+++ Gary-Blue/Order-Page/script.js
@@ -1,5 +1,104 @@
 var food_item = document.getElementById("food-item");
 var confirm_button = document.getElementById("confirm-quantity");
+var order_index = 1;
+
+var new_order_button = document.querySelector("#newOrderButton");
+
+function newOrder() {
+  {
+    order_index;
+  }
+  globalThis;
+  var orders = document.getElementById("orders");
+  var newOrder = document.createElement("div");
+  newOrder.innerHTML = `
+  <div>
+    <label for="foodDropdown"></label>
+    <select
+      name="foodDropdown"
+      id="foodDropdown"
+      class="item-select"
+      onchange="handleSelect(this.value, ${order_index})"
+    >
+      <option value="optionSelect" disabled selected>
+        Choose your option
+      </option>
+
+      <option value="main" disabled>------Main------</option>
+      <option value="The Big Gary">The Big Gary</option>
+      <option value="Regular Burger">Regular Burger</option>
+      <option value="Classic Cheeseburger">
+        Classic Cheeseburger
+      </option>
+      <option value="The Works Burger">The Works Burger</option>
+      <option value="Chili Burger">Chili Burger</option>
+      <option value="Juicy Lucy Burger">Juicy Lucy Burger</option>
+      <option value="Hot Dog">Hot Dog</option>
+      <option value="Chili Dog">Chili Dog</option>
+
+      <option value="breakfast" disabled>
+        ------Breakfast------
+      </option>
+      <option value="Scrambled Eggs">Scrambled Eggs</option>
+      <option value="Meat Omelette">Meat Omelette</option>
+      <option value="Veggie-Cheese Omelette">
+        Veggie-Cheese Omelette
+      </option>
+      <option value="Western Omelette">Western Omelette</option>
+      <option value="Eggs on Toast">Eggs on Toast</option>
+      <option value="Waffles">Waffles</option>
+      <option value="Pancakes">Pancakes</option>
+      <option value="Breakfast Burrito">Breakfast Burrito</option>
+
+      <option value="Sides" disabled>
+        ------Appetizers & Sides------
+      </option>
+      <option value="Classic Fries">Classic Fries</option>
+      <option value="Wings">Chicken Wings</option>
+      <option value="Nachos">Nachos</option>
+      <option value="Mozza Sticks">Mozza Sticks</option>
+
+      <option value="Beverages" disabled>
+        ------Beverages------
+      </option>
+      <option value="Beverage">Pepsi</option>
+      <option value="Beverage">7up</option>
+      <option value="Beverage">Sprite</option>
+      <option value="Beverage">Fanta</option>
+      <option value="Beverage">Root Beer</option>
+      <option value="Beverage">Lemonade</option>
+      <option value="Beverage">Soda Pop</option>
+      <option value="Beverage">Pepsi</option>
+
+      <option value="Milkshakes" disabled>
+        ------Shakes------
+      </option>
+      <option value="Shake">Vanilla Shake</option>
+      <option value="Shake">Strawberry Shake</option>
+      <option value="Shake">Cherry Shake</option>
+      <option value="Shake">Banana Split</option>
+      <option value="Cake">Cheesecake Slice</option>
+      <option value="Shake">Moomoo Milk</option>
+      <option value="Shake">Berry Juice</option>
+    </select>
+  </div>
+  <div>
+    <input
+      id="food-quantity"
+      type="number"
+      style="max-width: 3ch"
+      max="5"
+      min="0"
+      value="0"
+      onchange="quantitySelect(this.value, ${order_index})"
+    />
+  </div>
+  <div class="food-price">$0.00</div>
+  <div class="food-total">$0.00</div>`;
+  newOrder.classList.add("order");
+  orders.appendChild(newOrder);
+  order_index++;
+}
 
 function handleSelect(food_object, occurance) {
   var food_price = document.getElementsByClassName("food-price")[occurance];
@@ -87,7 +186,11 @@ function quantitySelect(num, occurance) {
 
 function calcSubTotal() {
   sub_total = 0;
-  for (i = 0; i <= 10; i++) {
+  {
+    order_index;
+  }
+  globalThis;
+  for (i = 0; i < order_index; i++) {
     var item_cost = document
       .getElementsByClassName("food-total")
       [i].innerHTML.replace("$", "");
